d048990c66ff6dd62e2dbdc7e589bd3d294a70ec
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-10-30 12:27:32 +0600

    Nesting fuctions

--- script.js
+++ script.js
@@ -8,5 +8,11 @@ const add = (a, b) => {
   return a + b;
 };
 
-const result = add(2, 2);
-console.log(result);
+// const result = add(2, 2);
+// console.log(result);
+
+const calculate = () => {
+  return add(2, 2);
+};
+
+calculate();
