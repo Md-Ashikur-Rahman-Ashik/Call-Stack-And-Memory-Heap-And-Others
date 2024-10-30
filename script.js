51417f4c2bece3681b36e2a4e459a1f566e2389e
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-10-30 12:34:37 +0600

    Learning about memory leak

--- script.js
+++ script.js
@@ -4,15 +4,21 @@
 
 // inception();
 
-const add = (a, b) => {
-  return a + b;
-};
+// const add = (a, b) => {
+//   return a + b;
+// };
 
 // const result = add(2, 2);
 // console.log(result);
 
-const calculate = () => {
-  return add(2, 2);
-};
+// const calculate = () => {
+//   return add(2, 2);
+// };
 
-calculate();
+// calculate();
+
+const arr = [];
+
+for (let i = 1; i > 0; i++) {
+  arr.push(i);
+}
