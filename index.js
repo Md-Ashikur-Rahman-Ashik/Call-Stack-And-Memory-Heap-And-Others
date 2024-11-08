539ec74bd8d3f75175e8f777f6f37140495fcdd5
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-11-08 10:54:53 +0600

    Explored prototype

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Car = function (brand, price, stock) {
   this.price = price;
   this.stock = stock;
 
+  //!   Avoid this type of action
   //   this.totalProfit = function(){
 
   //   }
@@ -18,5 +19,25 @@ const Car = function (brand, price, stock) {
 const myCar = new Car("Toyota", 10, 5);
 const h6 = new Car("Haval", 15, 2);
 
-console.log(myCar);
-console.log(h6);
+// console.log(myCar);
+// console.log(h6);
+
+Car.prototype.totalProfit = function () {
+  return this.price * this.stock;
+};
+
+// console.log(h6.totalProfit());
+// console.log(myCar.totalProfit());
+
+const arr = [1, 1, 5, 7, 9, 2, 8, 7, 3, 5, 5];
+
+// new Array = []
+
+// console.log(arr.__proto__);
+
+//! DON'T DO THIS KIND OF THINGS
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
