05e8503db56e65695a588ae63a5a3d2ea13799ae
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-11-15 18:05:44 +0600

    Exploring class inheritance

--- index.js
+++ index.js
@@ -185,29 +185,43 @@
 // book.call(hanif, ...bookingInfo);
 // console.log(hanif.bookings);
 
-const Person = function (name, birthYear) {
-  this.name = name;
-  this.birthYear = birthYear;
-};
+// const Person = function (name, birthYear) {
+//   this.name = name;
+//   this.birthYear = birthYear;
+// };
 
-Person.prototype.calculateAge = function (year) {
-  return year - this.birthYear;
-};
+// Person.prototype.calculateAge = function (year) {
+//   return year - this.birthYear;
+// };
 
-const mir = new Person("Mir", 1970);
+// const mir = new Person("Mir", 1970);
 // console.log(mir.calculateAge(2058));
 
-const Student = function (name, birthYear, subject) {
-  // Person(name, birthYear);
-  Person.call(this, name, birthYear);
-  this.subject = subject;
-};
+// const Student = function (name, birthYear, subject) {
+// Person(name, birthYear);
+// Person.call(this, name, birthYear);
+// this.subject = subject;
+// };
 
-Student.prototype = Object.create(Person.prototype);
+// Student.prototype = Object.create(Person.prototype);
 
-Student.prototype.intro = function () {
-  return `I'm ${this.name}`;
-};
+// Student.prototype.intro = function () {
+//   return `I'm ${this.name}`;
+// };
 
-const mezba = new Student("Mezba Abedin", 1965, "Science");
-console.log(mezba.calculateAge(2001));
+// const mezba = new Student("Mezba Abedin", 1965, "Science");
+// console.log(mezba.calculateAge(2001));
+
+class Person {
+  constructor(name, birthYear) {
+    this.name = name;
+    this.birthYear = birthYear;
+  }
+
+  calculateAge() {
+    return 2030 - this.birthYear;
+  }
+}
+
+const mizan = new Person("Mizan", 1980);
+console.log(mizan);
